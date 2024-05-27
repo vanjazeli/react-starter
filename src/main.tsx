9ca675631420a891b8d6de95516b573ac41d57b4
vanjazeli <vanjazeli@gmail.com>
2024-05-27 12:48:33 +0200

    feat: add sass and base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+import './styles/styles.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+
+window.addEventListener('load', () => {
+	document.documentElement.classList.add('loaded');
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
